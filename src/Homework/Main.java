ad03de4928d550efd9c21e44ef7c84adf42df703
JessyMakkry <jessy.makkry@bk.ru>
2024-06-15 14:15:23 +0300

    Commit 1

--- src/Homework/Main.java
+++ src/Homework/Main.java
@@ -1,5 +1,9 @@
+package Homework;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Task 1");
+        
+
     }
 }
