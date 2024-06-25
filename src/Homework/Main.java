86f021cc16a8a40d7065feefab769e3b4c114b2a
JessyMakkry <jessy.makkry@bk.ru>
2024-06-25 19:58:09 +0300

    Commit 1

--- src/Homework/Main.java
+++ src/Homework/Main.java
@@ -1,9 +1,26 @@
 package Homework;
 
 public class Main {
+    public static void printseparator(){
+        System.out.println("_____________________________________");
+    }
     public static void main(String[] args) {
         System.out.println("Task 1");
-        
-
+        Author Pushkin = new Author("Александр", "Пушкин");
+        Author Tolstoy = new Author("Лев", "Толстой");
+        Book theRedOne = new Book("Капитанская дочка", Pushkin, 1736);
+        Book theGreenOne = new Book("Война и мир", Tolstoy,1863);
+        System.out.println("theGreenOne.name = " + theGreenOne.getBookName());
+        System.out.println("theGreenOne.author.name = " + theGreenOne.getBookAuthorName());
+        System.out.println("theGreenOne.author.surname = " + theGreenOne.getBookAuthorSurname());
+        System.out.println("theGreenOne.year = " + theGreenOne.getBookYear());
+        printseparator();
+        System.out.println("theRedOne.name = " + theRedOne.getBookName());
+        System.out.println("theRedOne.author.name = " + theRedOne.getBookAuthorName());
+        System.out.println("theRedOne.author.surname = " + theRedOne.getBookAuthorSurname());
+        System.out.println("theRedOne.year = " + theRedOne.getBookYear());
+        printseparator();
+        theRedOne.setBookYear(1836);
+        System.out.println("theRedOne.getBookYear() = " + theRedOne.getBookYear());
     }
 }
